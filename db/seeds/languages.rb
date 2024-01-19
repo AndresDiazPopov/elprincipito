1909914277aa6833de354fb7e65497c67b855400
Andres Diaz Popov <elgalledesarrollo@gmail.com>
2024-01-19 07:18:41 +0100

    Cambios configuración

--- db/seeds/languages.rb
+++ db/seeds/languages.rb
@@ -1,5 +1,11 @@
-language_es = Language.create(name: 'Español', locale: 'es', state: 'enabled')
-language_es.translations.create(name: 'Spanish', locale: 'en')
+language_es = Language.find_by(name: 'Español')
+unless language_es
+  language_es = Language.create(name: 'Español', locale: 'es', state: 'enabled')
+  language_es.translations.create(name: 'Spanish', locale: 'en')
+end
 
-language_en = Language.create(name: 'Inglés', locale: 'es', state: 'enabled')
-language_en.translations.create(name: 'English', locale: 'en')
+language_en = Language.find_by(name: 'Inglés')
+unless language_en
+  language_en = Language.create(name: 'Inglés', locale: 'es', state: 'enabled')
+  language_en.translations.create(name: 'English', locale: 'en')
+end
